f65ca1478bdf1619e23ed1d52e484607e3d03e17
Nial Daly <ndaly500@hotmail.co.uk>
2018-01-21 19:57:55 +0000

    1

--- app.py
+++ app.py
@@ -309,6 +309,7 @@ def display_table(slider_1, slider_2, slider_3, slider_4, dropdown_3, input_1):
 
 
     #CALCULATES ENTHALPY AT STATE 2
+    dff3['H_2'] = dff3['H_2_ISENTROPIC'] + ((PUMP_EFF / 100)) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
     dff3['H_2'] = dff3['H_1'] + (PUMP_EFF / 100) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
 
     #CALCULATES ENTHALPY AT STATE 4
@@ -368,6 +369,7 @@ def produce_graph(slider_1, slider_2, slider_3, slider_4, dropdown_3, input_1):
     TURBINE_EFF = float(slider_4)
 
     #CALCULATES ENTHALPY AT STATE 2
+    dff3['H_2'] = dff3['H_2_ISENTROPIC'] + ((PUMP_EFF / 100)) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
     dff3['H_2'] = dff3['H_1'] + (PUMP_EFF / 100) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
 
     #CALCULATES ENTHALPY AT STATE 4
@@ -427,7 +429,8 @@ def produce_graph(slider_1, slider_2, slider_3, slider_4, dropdown_3, input_1):
     TURBINE_EFF = float(slider_4)
 
     #CALCULATES ENTHALPY AT STATE 2
-    dff3['H_2'] = dff3['H_1'] + (PUMP_EFF / 100) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
+    dff3['H_2'] = dff3['H_2_ISENTROPIC'] + ((PUMP_EFF / 100)) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
+    #dff3['H_2'] = dff3['H_1'] + (PUMP_EFF / 100) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
 
     #CALCULATES ENTHALPY AT STATE 4
     dff3['H_4'] = (dff3['H_3'] - (TURBINE_EFF / 100) * (dff3['H_3'] - dff3['H_4_ISENTROPIC']))
@@ -509,7 +512,8 @@ def produce_graph(slider_1, slider_2, slider_3, slider_4, dropdown_3, dropdown_4
 
     dff3 = dff2
     #CALCULATES ENTHALPY AT STATE 2
-    dff3['H_2'] = dff3['H_1'] + (PUMP_EFF / 100) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
+    dff3['H_2'] = dff3['H_2_ISENTROPIC'] + ((PUMP_EFF / 100)) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
+    #dff3['H_2'] = dff3['H_1'] + (PUMP_EFF / 100) * (dff3['H_2_ISENTROPIC'] - dff3['H_1'])
 
     #CALCULATES ENTHALPY AT STATE 4
     dff3['H_4'] = (dff3['H_3'] - ((TURBINE_EFF / 100) * (dff3['H_3'] - dff3['H_4_ISENTROPIC'])))
