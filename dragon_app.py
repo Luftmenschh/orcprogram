2e194e595863207fb4dcf41cd9736e42a0a28d8b
Nial Daly <ndaly500@hotmail.co.uk>
2018-01-29 20:54:16 +0000

    1

--- dragon_app.py
+++ dragon_app.py
@@ -73,6 +73,7 @@ app.layout = html.Div(
             ],
                 className='six columns',
             ),
+            html.Label('Athlete Indicators'),
             html.Div([
                 dt.DataTable(
                     rows=[{}],
@@ -82,50 +83,12 @@ app.layout = html.Div(
         selected_row_indices=[],
         id='table'
     ),
-
-                html.Label('Option settings:'),
-                dcc.RadioItems(
-                    id='option_selector',
-                    options=[
-                        {'label': 'Call', 'value': 'call'},
-                        {'label': 'Put', 'value': 'put'},
-                    ],
-                    value='call',
-                    labelStyle={'display': 'inline-block'},
-                ),
-                dcc.RadioItems(
-                    id='market_selector',
-                    options=[
-                        {'label': 'Market', 'value': 'market'},
-                        {'label': 'Last', 'value': 'last'},
-                    ],
-                    value='market',
-                    labelStyle={'display': 'inline-block'},
-                ),
             ],
                 className='two columns',
             ),
             html.Div([
-                html.Div([
-                    html.Label('Price threshold:'),
-                    dcc.Slider(
-                        id='price_slider',
-                        min=0,
-                        max=200,
-                        value=200,
-                    ),
-                ]),
-                html.Div([
-                    html.Label('Volume threshold:'),
-                    dcc.Slider(
-                        id='volume_slider',
-                        min=0,
-                        max=10,
-                        value=1,
-                    )
-                ]),
             ],
-                className='four columns'
+                className='five columns'
             ),
         ],
             className='row',
@@ -275,299 +238,8 @@ def cache_raw_data(ticker):
     return 'loaded'
 
 
-# Cache filtered data
-@app.callback(Output('filtered_container', 'hidden'),
-              [Input('raw_container', 'hidden'),
-               Input('option_selector', 'value'),
-               Input('market_selector', 'value'),
-               Input('price_slider', 'value'),
-               Input('volume_slider', 'value'),
-               Input('iv_selector', 'value'),
-               Input('calendar_selector', 'value'),
-               Input('rf_input', 'value'),
-               Input('div_input', 'value')])  # To be split
-def cache_filtered_data(hidden, call_or_put, market,
-                        above_below, volume_threshold,
-                        calculate_iv, trading_calendar,
-                        rf_interest_rate, dividend_rate):
-
-    if hidden == 'loaded':
-
-        if call_or_put == 'call':
-            s, p, i = get_filtered_data(raw_data, calculate_iv=calculate_iv,
-                                        call=True, put=False,
-                                        above_below=float(above_below),
-                                        volume_threshold=float(volume_threshold),
-                                        rf_interest_rate=float(rf_interest_rate),
-                                        dividend_rate=float(dividend_rate),
-                                        trading_calendar=trading_calendar,
-                                        market=market)
-        else:
-            s, p, i = get_filtered_data(raw_data, calculate_iv=calculate_iv,
-                                        call=False, put=True,
-                                        above_below=float(above_below),
-                                        volume_threshold=float(volume_threshold),
-                                        rf_interest_rate=float(rf_interest_rate),
-                                        dividend_rate=float(dividend_rate),
-                                        trading_calendar=trading_calendar,
-                                        market=market)
-
-        df = pd.DataFrame([s, p, i]).T
-
-        global filtered_data
-        filtered_data = df[df[2] > 0.0001]  # Filter invalid calculations with abnormally low IV
-        print('Loaded filtered data')
-
-        return 'loaded'
 
 
-# Make main surface plot
-@app.callback(Output('iv_surface', 'figure'),
-              [Input('filtered_container', 'hidden'),
-               Input('ticker_dropdown', 'value'),
-               Input('log_selector', 'value'),
-               Input('graph_toggles', 'values')],
-              [State('graph_toggles', 'values'),
-               State('iv_surface', 'relayoutData')])
-def make_surface_plot(hidden, ticker, log_selector, graph_toggles,
-                      graph_toggles_state, iv_surface_layout):
-
-    if hidden == 'loaded':
-
-        if 'flat' in graph_toggles:
-            flat_shading = True
-        else:
-            flat_shading = False
-
-        trace1 = {
-            "type": "mesh3d",
-            'x': filtered_data[0],
-            'y': filtered_data[1],
-            'z': filtered_data[2],
-            'intensity': filtered_data[2],
-            'autocolorscale': False,
-            "colorscale": [
-                [0, "rgb(244,236,21)"], [0.3, "rgb(249,210,41)"], [0.4, "rgb(134,191,118)"], [
-                    0.5, "rgb(37,180,167)"], [0.65, "rgb(17,123,215)"], [1, "rgb(54,50,153)"],
-            ],
-            "lighting": {
-                "ambient": 1,
-                "diffuse": 0.9,
-                "fresnel": 0.5,
-                "roughness": 0.9,
-                "specular": 2
-            },
-            "flatshading": flat_shading,
-            "reversescale": True,
-        }
-
-        layout = {
-            "title": "{} Volatility Surface | {}".format(ticker, str(dt.datetime.now())),
-            'margin': {
-                'l': 10,
-                'r': 10,
-                'b': 10,
-                't': 60,
-            },
-            'paper_bgcolor': '#FAFAFA',
-            "hovermode": "closest",
-            "scene": {
-                "aspectmode": "manual",
-                "aspectratio": {
-                    "x": 2,
-                    "y": 2,
-                    "z": 1
-                },
-                'camera': {
-                    'up': {'x': 0, 'y': 0, 'z': 1},
-                    'center': {'x': 0, 'y': 0, 'z': 0},
-                    'eye': {'x': 1, 'y': 1, 'z': 0.5},
-                },
-                "xaxis": {
-                    "title": "Strike ($)",
-                    "showbackground": True,
-                    "backgroundcolor": "rgb(230, 230,230)",
-                    "gridcolor": "rgb(255, 255, 255)",
-                    "zerolinecolor": "rgb(255, 255, 255)"
-                },
-                "yaxis": {
-                    "title": "Expiry (days)",
-                    "showbackground": True,
-                    "backgroundcolor": "rgb(230, 230,230)",
-                    "gridcolor": "rgb(255, 255, 255)",
-                    "zerolinecolor": "rgb(255, 255, 255)"
-                },
-                "zaxis": {
-                    "rangemode": "tozero",
-                    "title": "IV (σ)",
-                    "type": log_selector,
-                    "showbackground": True,
-                    "backgroundcolor": "rgb(230, 230,230)",
-                    "gridcolor": "rgb(255, 255, 255)",
-                    "zerolinecolor": "rgb(255, 255, 255)"
-                }
-            },
-        }
-
-        if (iv_surface_layout is not None and 'lock' in graph_toggles_state):
-
-            try:
-                up = iv_surface_layout['scene']['up']
-                center = iv_surface_layout['scene']['center']
-                eye = iv_surface_layout['scene']['eye']
-                layout['scene']['camera']['up'] = up
-                layout['scene']['camera']['center'] = center
-                layout['scene']['camera']['eye'] = eye
-            except:
-                pass
-
-        data = [trace1]
-        figure = dict(data=data, layout=layout)
-        return figure
-
-
-# Make side heatmap plot
-@app.callback(Output('iv_heatmap', 'figure'),
-              [Input('filtered_container', 'hidden'),
-               Input('ticker_dropdown', 'value'),
-               Input('graph_toggles', 'values')],
-              [State('graph_toggles', 'values'),
-               State('iv_heatmap', 'relayoutData')])
-def make_surface_plot(hidden, ticker, graph_toggles,
-                      graph_toggles_state, iv_heatmap_layout):
-
-    if hidden == 'loaded':
-
-        if 'discrete' in graph_toggles:
-            shading = 'contour'
-        else:
-            shading = 'heatmap'
-
-        trace1 = {
-            "type": "contour",
-            'x': filtered_data[0],
-            'y': filtered_data[1],
-            'z': filtered_data[2],
-            'connectgaps': True,
-            'line': {'smoothing': '1'},
-            'contours': {'coloring': shading},
-            'autocolorscale': False,
-            "colorscale": [
-                [0, "rgb(244,236,21)"], [0.3, "rgb(249,210,41)"], [0.4, "rgb(134,191,118)"],
-                [0.5, "rgb(37,180,167)"], [0.65, "rgb(17,123,215)"], [1, "rgb(54,50,153)"],
-            ],
-            # Add colorscale log
-            "reversescale": True,
-        }
-
-        layout = {
-            'margin': {
-                'l': 60,
-                'r': 10,
-                'b': 60,
-                't': 10,
-            },
-            'paper_bgcolor': '#FAFAFA',
-            "hovermode": "closest",
-            "xaxis": {
-                'range': [],
-                "title": "Strike ($)",
-            },
-            "yaxis": {
-                'range': [],
-                "title": "Expiry (days)",
-            },
-        }
-
-        if (iv_heatmap_layout is not None and 'lock' in graph_toggles_state):
-
-            try:
-                x_range_left = iv_heatmap_layout['xaxis.range[0]']
-                x_range_right = iv_heatmap_layout['xaxis.range[1]']
-                layout['xaxis']['range'] = [x_range_left, x_range_right]
-            except:
-                pass
-
-            try:
-                y_range_left = iv_heatmap_layout['yaxis.range[0]']
-                y_range_right = iv_heatmap_layout['yaxis.range[1]']
-                layout['yaxis']['range'] = [x_range_left, x_range_right]
-            except:
-                pass
-
-        data = [trace1]
-        figure = dict(data=data, layout=layout)
-        return figure
-
-
-# Make side scatter plot
-@app.callback(Output('iv_scatter', 'figure'),
-              [Input('filtered_container', 'hidden'),
-               Input('ticker_dropdown', 'value'),
-               Input('graph_toggles', 'values')],
-              [State('graph_toggles', 'values'),
-               State('iv_scatter', 'relayoutData')])
-def make_scatter_plot(hidden, ticker, graph_toggles,
-                      graph_toggles_state, iv_scatter_layout):
-
-    if hidden == 'loaded':
-
-        if 'discrete' in graph_toggles:
-            shading = 'contour'
-        else:
-            shading = 'heatmap'
-
-        if 'box' in graph_toggles:
-            typ = 'box'
-        else:
-            typ = 'scatter'
-
-        trace1 = {
-            "type": typ,
-            'mode': 'markers',
-            'x': filtered_data[1],
-            'y': filtered_data[2],
-            'boxpoints': 'outliers',
-            'marker': {'color': '#32399F', 'opacity': 0.2}
-        }
-
-        layout = {
-            'margin': {
-                'l': 60,
-                'r': 10,
-                'b': 60,
-                't': 10,
-            },
-            'paper_bgcolor': '#FAFAFA',
-            "hovermode": "closest",
-            "xaxis": {
-                "title": "Expiry (days)",
-            },
-            "yaxis": {
-                "rangemode": "tozero",
-                "title": "IV (σ)",
-            },
-        }
-
-        if (iv_scatter_layout is not None and 'lock' in graph_toggles_state):
-
-            try:
-                x_range_left = iv_scatter_layout['xaxis.range[0]']
-                x_range_right = iv_scatter_layout['xaxis.range[1]']
-                layout['xaxis']['range'] = [x_range_left, x_range_right]
-            except:
-                pass
-
-            try:
-                y_range_left = iv_scatter_layout['yaxis.range[0]']
-                y_range_right = iv_scatter_layout['yaxis.range[1]']
-                layout['yaxis']['range'] = [x_range_left, x_range_right]
-            except:
-                pass
-
-        data = [trace1]
-        figure = dict(data=data, layout=layout)
-        return figure
 
 @app.callback(
     dash.dependencies.Output('table', 'rows'),
@@ -588,7 +260,7 @@ def display_table(dropdown_1):
     dff3 = dff2
 
     #
-    dff3 = dff3[['Athlete', 'Age', 'Height', 'Weight (kg)']]
+    dff3 = dff3[['Athlete', 'Height', 'Weight (kg)']]
 
     return dff3.to_dict('records')
 
